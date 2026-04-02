4c564357d27fc83a9644a35a8b1ed9a6accc8195
Md Taufique Alam <taufiquealam2404@gmail.com>
2026-04-02 17:13:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
- function convertToRoman(num) {
-    if (num === 0) return "";
+function toRoman(num) {
+  if (num <= 0) return "";
 
-    const values = [
-        [1000, "M"],
-        [900, "CM"],
-        [500, "D"],
-        [400, "CD"],
-        [100, "C"],
-        [90, "XC"],
-        [50, "L"],
-        [40, "XL"],
-        [10, "X"],
-        [9, "IX"],
-        [5, "V"],
-        [4, "IV"],
-        [1, "I"]
-    ];
+  const romanMap = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
-    let result = "";
+  let result = "";
 
-    for (let [value, symbol] of values) {
-        while (num >= value) {
-            result += symbol;
-            num -= value;
-        }
+  for (let [symbol, value] of romanMap) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
     }
+  }
 
-    return result;
-} //your code here
-
+  return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+function convert() {
+  const input = document.getElementById("number").value;
+  const output = document.getElementById("output");
 
+  const num = parseInt(input);
 
+  if (isNaN(num) || num < 0) {
+    output.textContent = "Please enter a valid number (>= 0)";
+    return;
+  }
 
-
+  output.textContent = toRoman(num);
+}
 // do not edit below this line
 module.exports = convertToRoman
